cde3a0ba050d9b89e91a62e0b06f816913f0be19
Mandar899 <mandarmanvi899@gmail.com>
2022-03-26 18:53:56 +0530

    Add homepage

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="flex items-center justify-center h-screen bg-[#0C0C0C]">
       <Head>
         <title>Devbox</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-7xl font-semibold">Devbox</h1>
+      <div className="flex flex-col items-center justify-center space-y-10">
+        <h1 className="font-bold text-white uppercase text-8xl">
+          <span className="text-[#E5202B]">Dev</span>box
+        </h1>
+
+        <div className="flex items-center space-x-10">
+          <button className="btn">Register</button>
+          <button className="btn">Login</button>
+        </div>
+      </div>
     </div>
   );
 }
